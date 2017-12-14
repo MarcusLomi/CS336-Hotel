dfde4d8f0c5ba58945d5e87cbcabcdbeaa9f9b92
Marcus Lomi <marc.a.lomi@gmail.com>
2017-12-13 23:39:21 -0500

    final backup before submission deadline

--- src/app/reviewitem/roomReview.model.ts
+++ src/app/reviewitem/roomReview.model.ts
@@ -16,6 +16,7 @@ export class RoomReview {
         cid: string) {
             this.hotel_id = hotel_id;
             this.reviewId = reviewId;
+            this.rating = rating;
             this.room_no = room_no;
             this.text_comment = text_comment;
             this.cid = cid;
